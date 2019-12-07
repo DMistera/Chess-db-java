38a2fc42b38ad8a410d062178fdaf116c90f6650
dmistera2 <dmistera2@gmail.com>
2019-12-07 22:12:01 +0100

    second commit

---
+++ src/main/java/com/chessdb/ChessDbApplication.java
@@ -0,0 +1,13 @@
+package com.chessdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chessdb/ChessDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
